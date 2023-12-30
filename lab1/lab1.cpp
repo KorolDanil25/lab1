c2bc331941e4b40de7637af0b09df25954888995
king1 <king1@DESKTOP-FV81AO6>
2023-12-30 21:10:00 +0300

    Калькулятор без проверки на числа

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -2,14 +2,51 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "RUS");
+    double a;
+    double b;
+    int n;
+    double sum;
+
+    cout << "ВВедите первое число" << endl;
+    cin >> a;
+
+    cout << "ВВедите второе число" << endl;
+    cin >> b;
+
+    cout << "Выберите \n 1. Сложения  \n 2. Вычитание \n 3. Умножение \n 4. Деление" << endl;
+    cout << "----------------------------------------- " << endl;
+
+    cout << "Выбраное число" << endl;
+    cin >> n;
+    cout << "----------------------------------------- " << endl;
+
+    if (n == 1) {
+        sum = a + b;
+        cout << a <<"+" << b << endl;
+    } 
+    else  if (n == 2) {
+        sum = a - b;
+        cout << a << "-" << b << endl;
+    }
+    else if (n == 3) {
+        sum = a * b;
+        cout << a << "*" << b << endl;
+    }
+    else if (n == 4) {
+        sum = a / b;
+        cout << a << "/" << b << endl;
+    }
+    cout << "----------------------------------------- " << endl;
+    cout << "Результат будет : " << sum << endl;
     
-    cout << "Привет мир" << endl;
 }
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
